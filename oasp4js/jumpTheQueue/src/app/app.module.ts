c330cb7a7f6ee9562642c53540529740985cea28
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2017-11-16 17:08:09 +0100

    Add some comments

--- oasp4js/jumpTheQueue/src/app/app.module.ts
+++ oasp4js/jumpTheQueue/src/app/app.module.ts
@@ -1,8 +1,11 @@
+// Covalent imports
 import {
   CovalentLayoutModule,
   CovalentCommonModule,
   CovalentDataTableModule
 } from '@covalent/core';
+
+// Material imports
 import {
   MatCardModule,
   MatInputModule,
@@ -14,6 +17,7 @@ import {
 } from '@angular/material';
 import { CdkTableModule } from '@angular/cdk/table';
 
+// Angular core imports
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -21,6 +25,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import 'HammerJS';
 
+// Application components and services
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AccessComponent } from './access/access.component';
