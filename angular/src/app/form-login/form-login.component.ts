2a4b49ba999da5560407c586e7b281190abb5e65
Santos Jiménez Linares <santos.jimenez-linares@capgemini.com>
2022-03-29 13:25:21 +0200

    Fixed linting errors

--- angular/src/app/form-login/form-login.component.ts
+++ angular/src/app/form-login/form-login.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-form-login',
-  templateUrl: './form-login.component.html',
-  styleUrls: ['./form-login.component.scss'],
+  selector: "app-form-login",
+  templateUrl: "./form-login.component.html",
+  styleUrls: ["./form-login.component.scss"],
 })
-export class FormLoginComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class FormLoginComponent {}
