2a49dd091ff3d0272d6378e4b758bb371a3797b6
Shu Zhao <szx852@gmail.com>
2024-01-15 12:08:44 +0100

    fix yarn command test6 added wget6

--- angular/src/karma.conf.js
+++ angular/src/karma.conf.js
@@ -31,7 +31,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+  
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true,
   });
